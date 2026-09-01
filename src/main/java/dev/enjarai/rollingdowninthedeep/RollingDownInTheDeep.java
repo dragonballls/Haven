59c23c67eecc87e2375db259293bccc80d6adc17
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-01 08:47:48 -0700

    Fix duplicate key registration and 26.2 key mapping import usage

--- src/main/java/dev/enjarai/rollingdowninthedeep/RollingDownInTheDeep.java
+++ src/main/java/dev/enjarai/rollingdowninthedeep/RollingDownInTheDeep.java
@@ -2,7 +2,6 @@ package dev.enjarai.rollingdowninthedeep;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.Identifier;
@@ -18,7 +17,6 @@ public final class RollingDownInTheDeep implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         SwimConfig.INSTANCE.load();
-        KeyBindingHelper.registerKeyMapping(SwimKeybindings.TOGGLE_ENABLED);
         ClientTickEvents.END_CLIENT_TICK.register(SwimKeybindings::tick);
 
         SWIM_GROUP.trueIf(RollingDownInTheDeep::shouldRoll);
